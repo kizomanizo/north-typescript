965140a1fd057a80cd087193d01d638620edca71
Kizito Mrema <kizomanizo@gmail.com>
2023-08-14 19:25:22 +0300

    Fixed Prisma

--- dist/helpers/catchAsync.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const catchAsync = (fn) => (req, res, next) => {
-    return Promise.resolve(fn(req, res, next)).catch(next);
-};
-exports.default = catchAsync;
-//# sourceMappingURL=catchAsync.js.map
